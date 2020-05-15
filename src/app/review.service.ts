85e2eef611d504508f94a093400d413a5874627a
David Konecny <david4konecny@gmail.com>
2020-05-15 13:30:51 +0200

    enable saving todays review

--- src/app/review.service.ts
+++ src/app/review.service.ts
@@ -11,9 +11,40 @@ export class ReviewService {
     this.populateSampleData();
   }
 
+  getReviewByDate(date: Date) {
+    return this.reviews.find(it => this.datesEqual(it.date, date));
+  }
+
+  saveReview(review: Review) {
+    if (review.id) {
+      this.editReview(review);
+    } else {
+      this.addReview(review);
+    }
+  }
+
+  addReview(review: Review) {
+    review.id = this.genId();
+    this.reviews.push(review);
+  }
+
+  editReview(review: Review) {
+    const origReview = this.reviews.find(it => it.id === review.id);
+    origReview.rating = review.rating;
+    origReview.description = review.description;
+  }
+
+  genId(): number {
+    return this.reviews.length > 0 ? Math.max(...this.reviews.map(review => review.id)) + 1 : 1;
+  }
+
+  datesEqual(d1: Date, d2: Date): boolean {
+    return (d1.getFullYear() === d2.getFullYear() && d1.getMonth() === d2.getMonth() && d1.getDay() === d2.getDay());
+  }
+
   private populateSampleData() {
     this.reviews = [
-      { id: 1, date: new Date(2020, 5, 10), rating: 2, description: 'good' }
+      { id: 1, date: new Date(2020, 5, 10), rating: 2, description: 'good' },
     ];
   }
 }
--- src/app/review/review.component.ts
+++ src/app/review/review.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { ReviewService } from '../review.service';
+import { Review } from '../model/review';
 
 @Component({
   selector: 'app-review',
@@ -9,6 +10,7 @@ import { ReviewService } from '../review.service';
 })
 export class ReviewComponent implements OnInit {
   reviewForm: FormGroup;
+  review: Review;
 
   constructor(
     private reviewService: ReviewService,
@@ -16,16 +18,30 @@ export class ReviewComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.review = this.getTodayReview();
+    if (!this.review) {
+      this.review = { date: new Date(), rating: 3, description: ''} as Review;
+    }
+    this.setUpForm(this.review.rating, this.review.description);
+  }
+
+  getTodayReview() {
+    return this.reviewService.getReviewByDate(new Date());
+  }
+
+  setUpForm(rating: number, description: string) {
     this.reviewForm = this.formBuilder.group(
       {
-        rating: [3],
-        description: ['', Validators.required]
+        rating: [rating],
+        description: [description, Validators.required]
       }
     );
   }
 
   onSave() {
-
+    this.review.rating = this.reviewForm.value.rating;
+    this.review.description = this.reviewForm.value.description;
+    this.reviewService.saveReview(this.review);
   }
 
 }
