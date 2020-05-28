55e52b104c5979e509b00b49f49a3b6bba8b7d10
David Konecny <david4konecny@gmail.com>
2020-05-28 10:39:10 +0200

    add date navigation to time log

--- src/app/time/time-log/time-log.component.ts
+++ src/app/time/time-log/time-log.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TimeService } from '../../services/time.service';
 import { TimeSession } from '../../model/time-session';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-time-log',
@@ -11,6 +12,7 @@ export class TimeLogComponent implements OnInit {
   timeEntries: TimeSession[];
   displayedColumns = ['time', 'duration', 'edit'];
   date = new Date();
+  sub: Subscription;
 
   constructor(
     private timeService: TimeService
@@ -21,9 +23,24 @@ export class TimeLogComponent implements OnInit {
   }
 
   private loadTimeEntries() {
-    this.timeService.getEntriesByDate(this.date).subscribe(
+    this.sub = this.timeService.getEntriesByDate(this.date).subscribe(
       next => this.timeEntries = next
     );
+  }
+
+  private reloadTimeEntries() {
+    this.sub.unsubscribe();
+    this.loadTimeEntries();
+  }
+
+  onNextDay() {
+    this.date = this.timeService.getNextDay(this.date);
+    this.reloadTimeEntries();
+  }
+
+  onPreviousDay() {
+    this.date = this.timeService.getPreviousDay(this.date);
+    this.reloadTimeEntries();
   }
 
 }
