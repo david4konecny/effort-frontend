05f59be82af2694c32b67d64e2748cd37f8fd3a5
David Konecny <david4konecny@gmail.com>
2020-06-03 11:31:55 +0200

    fetch day totals in month

---
+++ src/app/stats.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {DateTotal} from './model/date-total';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StatsService {
+  private url = 'http://localhost:8080';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getDurationsForDaysInMonth(year: number, month: number): Observable<DateTotal[]> {
+    const monthFormatted = `${month}`.padStart(2, '0');
+    const options = { params: new HttpParams().set('date', `${year}-${monthFormatted}`) };
+    return this.http.get<DateTotal[]>(`${this.url}/time/total/month`, options);
+  }
+
+}
--- src/app/stats/month-stats/month-stats.component.ts
+++ src/app/stats/month-stats/month-stats.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {MatDatepicker, MatDatepickerInputEvent} from '@angular/material/datepicker';
+import { MatDatepicker } from '@angular/material/datepicker';
+import { StatsService } from '../../stats.service';
+import {Subscription} from 'rxjs';
+import {DateTotal} from '../../model/date-total';
 
 @Component({
   selector: 'app-month-stats',
@@ -7,15 +10,39 @@ import {MatDatepicker, MatDatepickerInputEvent} from '@angular/material/datepick
   styleUrls: ['./month-stats.component.css']
 })
 export class MonthStatsComponent implements OnInit {
+  labels = new Array<string>();
+  data = new Array<number>();
   year: number;
   month: number;
   date = new Date();
+  sub: Subscription;
 
-  constructor() { }
+  constructor(
+    private statsService: StatsService
+  ) { }
 
   ngOnInit(): void {
     this.year = this.date.getFullYear();
     this.month = this.date.getMonth() + 1;
+    this.reloadData();
+  }
+
+  private reloadData() {
+    if (this.sub) {
+      this.sub.unsubscribe();
+    }
+    this.sub = this.statsService.getDurationsForDaysInMonth(this.year, this.month).subscribe(
+      next => this.setDataset(next)
+    );
+  }
+
+  private setDataset(data: DateTotal[]) {
+    this.labels = [];
+    this.data = [];
+    data.forEach(item => {
+      this.labels.push(item.date);
+      this.data.push(item.total);
+    });
   }
 
   setDate() {
@@ -27,21 +54,24 @@ export class MonthStatsComponent implements OnInit {
   onPreviousPeriodClick() {
     this.setPreviousMonth();
     this.setDate();
+    this.reloadData();
   }
 
   onNextPeriodClick() {
     this.setNextMonth();
     this.setDate();
+    this.reloadData();
   }
 
   onMonthSelected(change: Date, picker: MatDatepicker<Date>) {
+    picker.close();
     this.year = change.getFullYear();
     this.month = change.getMonth() + 1;
     this.setDate();
-    picker.close();
+    this.reloadData();
   }
 
-  setPreviousMonth() {
+  private setPreviousMonth() {
     if (this.month === 1) {
       this.year--;
       this.month = 12;
@@ -50,7 +80,7 @@ export class MonthStatsComponent implements OnInit {
     }
   }
 
-  setNextMonth() {
+  private setNextMonth() {
     if (this.month === 12) {
       this.year++;
       this.month = 1;
