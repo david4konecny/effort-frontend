bc108f2d2c0f311d1b10dc7b6f31a1d023bc0484
David Konecny <david4konecny@gmail.com>
2020-06-07 14:02:03 +0200

    add AuthGuard and AuthService

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,13 @@ import { StatsComponent } from './stats/stats.component';
 import { HomeComponent } from './home/home.component';
 import { TodayComponent } from './today/today.component';
 import { LoginComponent } from './auth/login/login.component';
+import { AuthGuard } from './auth/auth.guard';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent, children: [
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard], children: [
       { path: '', children: [
           { path: '', component: TodayComponent },
           { path: 'stats', component: StatsComponent },
